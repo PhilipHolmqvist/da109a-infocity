1ec697416d29e57e1f1fc8f2b456b32625c1f0b6
Philip <philip_holmqvist@hotmail.com>
2022-12-15 16:03:17 +0100

    api klasser

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-#hej
+# -*- coding: utf-8 -*-
+from flask import Flask, request
+
+app = Flask(__name__)
+setup() #Nödvändiga saker som ska göras när servern startar.
+
+
+# Definera ändpunkter för de olika API metoderna.
+@app.route("/", methods=['GET']) #Lista alla enhörningar
+def list_cities(): 
+
+   
+
+        return "" #Begäran vill ha svar i HTML
+
+
+#Tillfällig frontpage route.
+@app.route("/frontend", methods=['GET'])
+def frontpage():
+    return(render_template("index.html"))
+
+
+    
