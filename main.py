510e815373bd17e4799c1031ca1e95badfd0edf4
Philip <philip_holmqvist@hotmail.com>
2022-12-15 16:20:54 +0100

    filer

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, request
 
+
+# python -m venv myenv --för att skapa en ny virtuell miljö
+# $env:FLASK_APP = "main.py"  --berätta för flask var applikation finnns
+# flask run --kör servern
+
+
 app = Flask(__name__)
-setup() #Nödvändiga saker som ska göras när servern startar.
+ # setup(): Nödvändiga saker som ska göras när servern startar.
 
 
 # Definera ändpunkter för de olika API metoderna.
-@app.route("/", methods=['GET']) #Lista alla enhörningar
-def list_cities(): 
+@app.route("/<input>", methods=['GET']) #Lista alla enhörningar
+def list_cities(input): 
 
    
 
-        return "" #Begäran vill ha svar i HTML
+        return "The input was: " + str(input) #Begäran vill ha svar i HTML
 
 
 #Tillfällig frontpage route.
