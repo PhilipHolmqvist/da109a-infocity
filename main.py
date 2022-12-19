6fb93c02aa708f26b15772a99b56fcefd2c36a90
Philip <philip_holmqvist@hotmail.com>
2022-12-19 12:54:52 +0100

    GeoDB

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 from flask import Flask, request
 from flask import render_template
 
+from APIer.geodb import get_cityDetails
+
 # python -m venv myenv --för att skapa en ny virtuell miljö
 # $env:FLASK_APP = "main.py"  --berätta för flask var applikation finnns
 # flask run --kör servern
@@ -9,13 +11,14 @@ from flask import render_template
 app = Flask(__name__)
  # setup(): Nödvändiga saker som ska göras när servern startar.
 
+hej = get_cityDetails("Malmö")
 # Definera ändpunkter för de olika API metoderna.
 #@app.route("/<input>", methods=['GET']) #Lista alla enhörningar
 #def list_cities(input): 
  #       return "The input was: " + str(input) #Begäran vill ha svar i HTML
 
 #Tillfällig frontpage route.
-@app.route("/frontend", methods=['GET'])
+@app.route("/<Cityname>", methods=['GET'])
 def frontpage():
         return(render_template("index.html"))
 
@@ -33,4 +36,4 @@ def style():
 def script():
     return render_template('script.js')
 
-app.run(debug=True)
+#app.run(debug=True)
