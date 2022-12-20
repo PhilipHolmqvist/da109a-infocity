091f795b7fd9ffffbaf2d53f22a9be66f03310ff
IHasFingers <redhawksphilip@gmail.com>
2022-12-20 12:08:08 +0100

    la till infosida
    info

--- static/script.js
+++ static/script.js
@@ -41,5 +41,12 @@ $(document).ready(function () {
         });
 
     });
+    const toggleButton = document.querySelector('.toggle-button');
+    const menu = document.querySelector('.menu');
+
+    toggleButton.addEventListener('click', () => {
+      menu.classList.toggle('active');
+    });
+
 
 });
