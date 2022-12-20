d21bf8fb09a3e55ef990e09d208879cdd3d58a06
SossioG <sosiosos2002@gmail.com>
2022-12-20 13:02:13 +0100

    weather.py update

--- APIer/weather.py
+++ APIer/weather.py
@@ -4,13 +4,13 @@ import json
 #Author: Sossio Giorgelli, version 1
 
 def get_cityWeather(countryName, dayLimit): #dayLimit shall be 4
-    url = "https://weatherapi-com.p.rapidapi.com/future.json"
+    url = "https://weatherapi-com.p.rapidapi.com/forecast.json"
 
     querystring = {
         "q": countryName,
         "days": dayLimit
     }
-    
+
     headers = {
         "X-RapidAPI-Key": "0ab490636dmsh7da5c2757e98131p159b59jsn6b2e12133ae3",
         "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
@@ -22,5 +22,20 @@ def get_cityWeather(countryName, dayLimit): #dayLimit shall be 4
         headers=headers, 
         params=querystring
     )
+    
+    response_data = response.json()
 
-    print(response.text)
+    print(response_data['location']['name'] + ", " + response_data['location']['country'])
+
+    #for s in response_data['forecast']['forecastday'].keys():
+    #   print(("Min temp: " + response_data['day']['mintemp_c'] + " C, Max temp: " + response_data['day']['maxtemp_c'] + " C"))
+    
+    
+    print(response_data['forecast']['forecastday'][0]['date'])
+    print("Min temp: " + str(response_data['forecast']['forecastday'][0]['day']['mintemp_c']) + " C, Max temp: " + str(response_data['forecast']['forecastday'][0]['day']['maxtemp_c']) + " C")
+
+    print(response_data['forecast']['forecastday'][1]['date'])
+    print("Min temp: " + str(response_data['forecast']['forecastday'][1]['day']['mintemp_c']) + " C, Max temp: " + str(response_data['forecast']['forecastday'][1]['day']['maxtemp_c']) + " C")
+
+    print(response_data['forecast']['forecastday'][2]['date'])
+    print("Min temp: " + str(response_data['forecast']['forecastday'][2]['day']['mintemp_c']) + " C, Max temp: " + str(response_data['forecast']['forecastday'][2]['day']['maxtemp_c']) + " C")
